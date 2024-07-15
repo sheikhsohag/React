588131da7766536b35e5f22d3af1f74365d0a049
sheikhsohag <sheikhsohag000@gmail.com>
2024-07-15 15:06:05 +0600

    hooks-useState Hooks

---
+++ react-hooks/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Colorizer from './components/colorize'
+import './assets/css/app.css'
+
+function App() {
+
+
+  return (
+    <>
+         <div className='App'>
+             <Colorizer/>
+         </div>
+    </>
+  )
+}
+
+export default App
